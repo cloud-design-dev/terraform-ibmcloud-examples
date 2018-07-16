ead82aa7dd09d825f2cc7d1db5d8f476805e8ef1
greyhoundforty <ryantiffany@fastmail.com>
2018-07-16 10:12:48 -0500

    Adding Kubernetes example

---
+++ KubernetesMultiZoneCluster/main.tf
@@ -0,0 +1,23 @@
+resource "ibm_container_cluster" "rttest_cluster" {
+  name            = "rttestcluster"
+  datacenter      = "wdc06"
+  machine_type    = "u2c.2x4"
+  public_vlan_id  = "1669321"
+  private_vlan_id = "1669323"
+  worker_num      = 3
+  hardware        = "shared"
+  account_guid    = "${var.ibm_account_guid}"
+  org_guid        = "${var.ibm_org_guid}"
+  space_guid      = "${var.ibm_space_guid}"
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "new_zone" {
+  depends_on = ["ibm_container_cluster.rttest_cluster"]
+  count = 3
+  cluster = "rttestcluster"
+  worker_pool = "default"
+  zone = "${element(var.zones,count.index)}"
+  private_vlan_id = "${element(var.private_vlans,count.index)}"
+  public_vlan_id = "${element(var.public_vlans,count.index)}"
+}
+
---
+++ KubernetesMultiZoneCluster/vars.tf
@@ -0,0 +1,27 @@
+# IBM Cloud PaaS API Key:
+variable ibm_bx_api_key {}
+
+# IBM Cloud IaaS User (aka SoftLayer Username)
+variable ibm_sl_username {}
+
+# IBM Cloud IaaS User API key (aka SoftLayer User Api Key)
+variable ibm_sl_api_key {}
+
+variable ibm_account_guid {}
+variable ibm_org_guid {}
+variable ibm_space_guid {}
+
+variable zones {
+    type = "list"
+    default = ["wdc07","wdc04"]
+}
+
+variable private_vlans {
+type = "list"
+default = ["1892939", "2149535"]
+}
+
+variable public_vlans {
+type = "list"
+default = ["1892917", "2149537"]
+}
