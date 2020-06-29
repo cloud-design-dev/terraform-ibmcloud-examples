646845f1ec3982a67dd6e508bfde330852e6f072
greyhoundforty <ryantiffany@fastmail.com>
2020-06-29 14:13:47 -0500

    Added VLAN association to Juniper example
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- Juniper_vSRX/main.tf
+++ Juniper_vSRX/main.tf
@@ -15,7 +15,7 @@ resource "ibm_network_vlan" "ha_srx_private_vlan" {
 }
 
 resource "ibm_network_gateway" "ha_vsrx" {
-  name = "ha-srx-us-south"
+  name = "ha-srx-us-east"
 
   members {
     hostname             = "ha1-srx-${var.datacenter}"
@@ -61,3 +61,14 @@ resource "ibm_network_gateway" "ha_vsrx" {
   }
 }
 
+resource "ibm_network_gateway_vlan_association" "public_association" {
+  depends_on      = [ibm_network_gateway.ha_vsrx]
+  gateway_id      = ibm_network_gateway.ha_vsrx.id
+  network_vlan_id = ibm_network_vlan.ha_srx_public_vlan.id
+}
+
+resource "ibm_network_gateway_vlan_association" "private_association" {
+  depends_on      = [ibm_network_gateway.ha_vsrx]
+  gateway_id      = ibm_network_gateway.ha_vsrx.id
+  network_vlan_id = ibm_network_vlan.ha_srx_private_vlan.id
+}
