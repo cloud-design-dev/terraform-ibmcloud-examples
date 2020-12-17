02d5a8dc0f7665ed9bfca652ccb59957d9d65e33
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2020-12-17 16:38:18 -0600

    Update installer.sh

--- VPC-LB-PrivateDNS-Minio/instances/installer.sh
+++ VPC-LB-PrivateDNS-Minio/instances/installer.sh
@@ -11,7 +11,7 @@ DEBIAN_FRONTEND=noninteractive apt -qqy -o Dpkg::Options::='--force-confdef' -o 
 } >> "$installerlog" 2>&1
 
 checkin() {
-curl -X POST --data-urlencode "payload={\"channel\": \"#ibmcloud\", \"username\": \"webhookbot\", \"text\": \"System `hostname -s` has come online system with a primary ip of: `hostname -I`\"}, \"icon_emoji\": \":ghost:\"}" "https://hooks.slack.com/services/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"
+curl -X POST --data-urlencode "payload={\"channel\": \"#ibmcloud\", \"username\": \"webhookbot\", \"text\": \"System `hostname -s` has come online system with a primary ip of: `hostname -I`\"}, \"icon_emoji\": \":ghost:\"}" "${SLACK_URL}"
 } >> "$installerlog" 2>&1
 
 sys_update
